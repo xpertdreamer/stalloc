09e51d3f12137af9b05cfae370ee05c3aefb9b62
xpertdreamer <yungnomen@gmail.com>
2026-08-30 22:37:09 +0300

    Allocate one page of memory

--- lib.asm
+++ lib.asm
@@ -5,14 +5,36 @@ public _start
 extrn printf
 extrn exit
 
+macro call_brk argument{
+  mov rax, 12
+  mov rdi, argument
+  syscall
+}
+
+define PAGESIZE 0x1000
+  
 section '.data' writable
-  ; msg db "Hello, number %d!", 0xA, 0
+  heap_start dq 0x00
+  msg_heap_start db "brk_start: 0x%x", 0xA, 0
+  msg_heap_curr db "current brk: 0x%x", 0xA, 0
 
 section '.text' executable
 
 _start:
-  ; mov rdi, msg
-  ; mov rsi, 9
-  ; call printf
-  ; xor rdi, rdi
-  ; call exit
+  call_brk 0
+
+  mov rdi, msg_heap_start
+  mov [heap_start], rax
+  mov rsi, [heap_start]
+  call printf
+
+  mov rcx, [heap_start]
+  add rcx, PAGESIZE
+  call_brk rcx
+
+  mov rdi, msg_heap_curr
+  mov rsi, rax
+  call printf
+
+  xor rdi, rdi
+  call exit
