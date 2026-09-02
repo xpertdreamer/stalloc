65c6c6d2f83b22cd0774d05926a13457b0d37b5f
xpertdreamer <yungnomen@gmail.com>
2026-09-02 12:23:16 +0300

    TRACE macro to print value and address of variables

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define TRACE(var)                                                      \
+    do {                                                                \
+        printf("%s\t%d\t%p\n", #var, *var, (void*)var);                 \
+} while (0);
+
 extern void* stalloc(int size);
 
 int main(void) {
@@ -19,9 +24,11 @@ int main(void) {
       return 1;
   }
   *b = 38;
-  printf("a value:   \t%d\n", *a);
-  printf("a address: \t%p\n", (void *)a);
-  printf("b value:   \t%d\n", *b);
-  printf("b address: \t%p", (void*)b);
+  /* printf("a value:   \t%d\n", *a); */
+  /* printf("a address: \t%p\n", (void *)a); */
+  /* printf("b value:   \t%d\n", *b); */
+  /* printf("b address: \t%p", (void*)b); */
+  TRACE(a);
+  TRACE(b);
   return 0;
 }
